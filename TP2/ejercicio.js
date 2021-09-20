26de077ee20680195277a540f2be9adc80124f59
Martin Rados <radosm@gmail.com>
2021-09-20 16:08:18 -0300

    primera solucion

--- TP2/ejercicio.js
+++ TP2/ejercicio.js
@@ -10,8 +10,10 @@
 // Se deberá aplicar primero la escala, luego la rotación y finalmente la traslación. 
 // Las rotaciones vienen expresadas en grados. 
 function BuildTransform( positionX, positionY, rotation, scale )
-{
-	return Array(1,0,0,0,1,0,0,0,1);
+{	
+	return Array( scale*Math.cos(-rotation),scale*Math.sin(-rotation),0,
+	             -scale*Math.sin(-rotation),scale*Math.cos(-rotation),0,
+				  positionX               ,positionY               ,1);
 }
 
 // Esta función retorna una matriz que resula de la composición de trasn1 y trans2. Ambas 
@@ -20,7 +22,25 @@ function BuildTransform( positionX, positionY, rotation, scale )
 // primero trans1 y luego trans2. 
 function ComposeTransforms( trans1, trans2 )
 {
-	return Array(1,0,0,0,1,0,0,0,1);
+	return(productoEscalar(fila(trans2,0),columna(trans1,0))
+	,productoEscalar(fila(trans2,1),columna(trans1,0))
+	,productoEscalar(fila(trans2,2),columna(trans1,0))
+	,productoEscalar(fila(trans2,0),columna(trans1,1))
+	,productoEscalar(fila(trans2,1),columna(trans1,1))
+	,productoEscalar(fila(trans2,2),columna(trans1,1))
+	,productoEscalar(fila(trans2,0),columna(trans1,2))
+	,productoEscalar(fila(trans2,1),columna(trans1,2))
+	,productoEscalar(fila(trans2,2),columna(trans1,2)));
 }
 
+function fila( m, n) {
+	return m.filter((e,i)=>{return i%3==n});
+}
 
+function columna( m, n) {
+	return m.slice(3*n,3*n+3);
+}
+
+function productoEscalar(p,q){
+	return p.reduce((p,e,i)=>{ return p+e*q[i]},0);
+}
